f0db3a5fdde154562f47d3fcf9d9897b0a0eccf2
Jesse Bryan and Victor Puentes <victor@>
2017-07-12 10:24:54 -0700

    JS logic for words starting with a single consonant

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 var pigLatinTranslator = function(sentence) {
   if (vowels.includes(sentence.charAt(0))) {
     return (sentence + "way");
+  } else {
+    var sentenceArr = sentence.split('');
+    sentenceArr.push(sentenceArr[0]);
+    sentenceArr.shift();
+    sentence = sentenceArr.join('') + "ay";
+    return sentence;
   }
 }
 
